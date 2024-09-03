d0030303d6f43cf0f1bec89968a5c2362a4d43ec
Mauricio Belmonte <frontend@kuiraweb.com>
2024-09-02 18:01:50 -0600

    Terminación create

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,6 +3,44 @@
         <div class="max-w-7xl mx-auto sm:px-6 px-8 py-6 bg-white ">
 
             <form>
+                <div class="grid gap-6 my-4 md:grid-cols-2">
+                    <div class="profile_avatar flex justify-center">
+                        <img class="rounded-full w-[50%]" src="{{asset('img/Elliot.jpg')}}" alt="Extra small avatar">
+
+                        <div class="input_file_box">
+                            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3 9C3 7.89543 3.89543 7 5 7H6.5C7.12951 7 7.72229 6.70361 8.1 6.2L9.15 4.8C9.52771 4.29639 10.1205 4 10.75 4H13.25C13.8795 4 14.4723 4.29639 14.85 4.8L15.9 6.2C16.2777 6.70361 16.8705 7 17.5 7H19C20.1046 7 21 7.89543 21 9V18C21 19.1046 20.1046 20 19 20H5C3.89543 20 3 19.1046 3 18V9Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <circle cx="12" cy="13" r="4" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></circle> </g></svg>
+                        </div>
+                    </div>                
+                <div class="bg-white p-6 rounded-lg mb-6 w-full max-w-lg">
+                    <div class="mb-4">
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-600" for="file_input">Upload file</label>
+                        <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file">
+                        <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
+                        
+                    </div>
+            
+                    <div class="mb-4">
+                        <label class="block text-gray-700 font-medium mb-2">Assign Role</label>
+                        <div class="flex items-center mb-2">
+                            <input id="admin" name="role" type="radio"
+                                class="h-4 w-4 text-blue-600 border-gray-300 focus:ring-blue-500">
+                            <label for="admin" class="ml-2 block text-sm text-gray-700">Administrator</label>
+                        </div>
+                        <div class="flex items-center mb-2">
+                            <input id="member" name="role" type="radio"
+                                class="h-4 w-4 text-blue-600 border-gray-300 focus:ring-blue-500">
+                            <label for="member" class="ml-2 block text-sm text-gray-700">Member</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input id="viewer" name="role" type="radio" checked
+                                class="h-4 w-4 text-blue-600 border-gray-300 focus:ring-blue-500">
+                            <label for="viewer" class="ml-2 block text-sm text-gray-700">Viewer</label>
+                        </div>
+                    </div>
+            
+                </div>
+                    
+                </div>
                 <div class="grid gap-6 mb-6 md:grid-cols-2">
                     <div>
                         <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-600">Nombre</label>
@@ -51,8 +89,25 @@
                         placeholder="•••••••••" required />
                 </div>
 
-                <button type="submit"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crear Usuario</button>
+                <div class="mb-4 flex justify-end">
+                <label class="inline-flex items-center mb-5 cursor-pointer">
+                    <input type="checkbox" value="" class="sr-only peer" checked>
+                    <span class="mr-3 text-md font-medium text-gray-900 dark:text-gray-600">Estatus</span>
+                    <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                  </label>
+                </div>
+                <div class="flex justify-end">
+                    <button
+                        class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit">
+                        + Add new user
+                    </button>
+                    <button
+                        class="ml-2 bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="button">
+                        Discard
+                    </button>
+                </div>                
             </form>
 
         </div>
